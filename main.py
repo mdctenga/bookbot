dfcbed3864c5e3b0d3a63c3ea1a57fbd50457a6a
mdctenga <mdctenga@gmail.com>
2024-03-20 07:16:45 -1000

    created function to count words in a book

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ def main():
     with open("./books/frankenstein.txt") as f:
         file_contents = f.read()
         print(file_contents)
+def count_words():
+    with open("./books/frankenstein.txt") as f:
+        file_contents = f.read()
+        wordList = file_contents.split()
+        wordCount = 0
+        for word in wordList:
+            wordCount+= 1   
+        print(wordCount)
 
-main()
-
-    
+# main()
+count_words()
