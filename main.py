f640ea1b0a1aa091098e90ab13686ee24573e9c6
mdctenga <mdctenga@gmail.com>
2024-03-21 07:44:38 -1000

    add functionality to lowercase text string

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     count_words = get_count_words(text)
+    count_letters = get_count_letter(text)
     print(f"{count_words} words found in the document")
+    print(f"{count_letters} words found in the document")
     
 def get_book_text(path):
     with open(path) as f:
@@ -14,5 +16,16 @@ def get_count_words(text):
     for word in words:
         wordCount+= 1
     return wordCount
+
+def get_count_letter(text):
+    # lowercase
+    lowered_string = text.lower();
+    # split letters
+    letters = lowered_string.split()
+    # count letters and add to dictionary
+    
+    letterCount = 0
+    
+    return letters
 
 main()
