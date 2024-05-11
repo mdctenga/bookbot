eac320aadfeb803034239a1bc46835dcf2a67363
mdctenga <mdctenga@gmail.com>
2024-05-11 09:11:08 -1000

    filter non-letters, sort most frequently used letters, and print required output

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ def main():
     count_words = get_count_words(text)
     count_letters = get_count_letter(text)
     print(f"{count_words} words found in the document")
-    print(f"{count_letters} letters found in the document")
+    for letter_dict in count_letters:
+        char = letter_dict["char"]
+        num = letter_dict["num"]
+        print(f"The {char} character was found {num} times")
     
 def get_book_text(path):
     with open(path) as f:
@@ -26,11 +29,20 @@ def get_count_letter(text):
     lettersCount = {}
         
     for letter in letters:
-        if letter in lettersCount:
-            lettersCount[letter] += 1
-        else:
-            lettersCount[letter] = 1
+        if letter.isalpha():
+            if letter in lettersCount:
+                lettersCount[letter] += 1
+            else:
+                lettersCount[letter] = 1
+
+    letter_count_list = [{"char": letter, "num": lettersCount[letter]} for letter in lettersCount]
+ 
+
+    letter_count_list.sort(key=sort_on, reverse=True)
     
-    return lettersCount
+    return letter_count_list
+
+def sort_on(dict):
+    return dict["num"]
 
 main()
